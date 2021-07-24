7b4af11359c5e811a26657c031b15163bbb8c23d
Nguyễn Quang Tân <81596255+nguyenquangtan@users.noreply.github.com>
2021-07-24 15:54:01 +0700

    2 hàm insert extension & title

--- MiniSearchEngine/MiniSearchEngine/TrieFunction.cpp
+++ MiniSearchEngine/MiniSearchEngine/TrieFunction.cpp
@@ -24,6 +24,28 @@ void Trie::insert(string& s, int index, int position) {
 	}
 }
 
+void Trie::insertTitle(string& title, int index) {
+	Node* cur = root;
+	for (int i = 0; i < title.size(); i++) {
+		if (cur->children.find(title[i]) == cur->children.end()) {
+			cur->children[title[i]] = new Node(title[i], cur);
+		}
+		cur = cur->children[title[i]];
+	}
+	cur->inTitle.push_back(index);
+}
+
+void Trie::insertExtension(string& extension, int index) {
+	Node* cur = root;
+	for (int i = 0; i < extension.size(); i++) {
+		if (cur->children.find(extension[i]) == cur->children.end()) {
+			cur->children[extension[i]] = new Node(extension[i], cur);
+		}
+		cur = cur->children[extension[i]];
+	}
+	cur->fileType.push_back(index);
+}
+
 Node* Trie::newNode(string& s) {
 	Node* cur = root;
 	for (int i = 0; i < s.size(); i++) {
