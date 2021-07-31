15f2180f674d6514e34008fff4f091699e985213
Thuan Luu <thuanrt123@gmail.com>
2021-07-31 16:53:25 +0700

    .

--- MiniSearchEngine/MiniSearchEngine/Menu.cpp
+++ MiniSearchEngine/MiniSearchEngine/Menu.cpp
@@ -160,8 +160,8 @@ void UserInterface::input(Poro& PoroPoro) {
 		if (ch == ENTER) { 
 			gotoxy(X, Y+2);
 			cout << "                                    ";
+			gotoxy(X, Y + 2);
 			for (auto file : PoroPoro.search_trie->result) {
-				gotoxy(X, Y+2);
 				cout << file.index << ' ';
 			}
 			system("pause");
